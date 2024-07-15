f9cc9a883d1eafba3c26202af90a1030d3998a76
HAU BUI VAN <bvhau@misa-group.vn>
2024-07-15 15:04:00 +0700

    thay đoi cau truc thu muc

--- src/renderer/service/AccountService.ts
+++ src/renderer/service/AccountService.ts
@@ -1,6 +1,6 @@
 export class AccountService {
     getAccounts() {
-        return fetch('/demo/data/accounts.json', { headers: { 'Cache-Control': 'no-cache' } })
+        return fetch('demo/data/accounts.json', { headers: { 'Cache-Control': 'no-cache' } })
             .then((res) => res.json())
             .then((d) => d.data);
     }
